4c6d5ddca48be9adc72ac4a25979286ebb7defa1
LordWilliam <willjordan05@icloud.com>
2025-09-15 21:20:02 -0700

    WO: Mesh triangulation version 1 implemented.

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,4 +1,4 @@
-using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class HexGrid : MonoBehaviour
@@ -15,6 +15,35 @@ public class HexGrid : MonoBehaviour
 
     [SerializeField]
     public float HexSize;
+
+    [SerializeField]
+    public HexCell HexCell;
+
+    private HexCell[] hexCells;
+
+    private HexMesh hexMesh;
+
+    void Instantiate()
+    {
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        hexCells = new HexCell[Width * Height];
+
+        for (int z = 0, i = 0; z < Height; z++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Vector3 hexCenter = HexMath.GetHexCenter(HexSize, x, z, HexOrientation) + transform.position;
+                HexCell hexCell = Instantiate(HexCell, hexCenter, Quaternion.identity, this.transform);
+
+                hexCell.CenterPosition = hexCenter;
+
+                hexCells[i++] = hexCell;
+            }
+        }
+
+        hexMesh.Triangulate(hexCells, HexSize, HexOrientation);
+    }
 
     void OnDrawGizmos()
     {
@@ -40,18 +69,6 @@ public class HexGrid : MonoBehaviour
 
     void Awake()
     {
-
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate();
     }
 }
